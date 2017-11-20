b1ce092e3f39585493960b52e84cbefe8e05e3a4
Chris Howes <chrishowes36@gmail.com>
2017-11-20 18:17:09 +0000

    Now bad guesses are handled!

--- SpoofMultiplayer.py
+++ SpoofMultiplayer.py
@@ -14,10 +14,11 @@ class Player:
     def chooseCoins(self, coins):
         self.coins = coins
 
-numberOfPlayers = int(input("How many players do you want?"))
+
+numberOfPlayers = int(input("How many players do you want? "))
 players = []
 for i in range(0, numberOfPlayers):
-    name = input("Hello, Player! What should I call you?")
+    name = input("Hello, Player! What should I call you? ")
     players.append(Player(name))
     
 
@@ -26,22 +27,40 @@ rounds = int(input("How many rounds would you like to play? "))
 for i in range(0, rounds):
     for j in range(0, len(players)):
         print("Hello, " + players[j].name)
-        coins = input("How many coins do you have?")
+        coins = int(input("How many coins do you have? "))
         players[j].chooseCoins(coins)
         
-        guess = input("How many coins do you think there are?")
+        guess = int(input("How many coins do you think there are? "))
+        guessing = True
+        while guessing:
+            print("got to while")
+            if j == 0 and not guess < players[j].coins and not guess > (6 - (guess - 3)):
+                guessing = False
+            elif j == 0:
+                print("Invalid Guess! That is a silly mistake young person!")
+                guess = int(input("How many coins do you think there are? "))
+            else:
+                for k in range(0, j):
+                    print("got to for")
+                    if players[k].guess == guess or guess < players[j].coins or guess > (6 - (guess - 3)):
+                        print("Invalid guess!")
+                        guess = int(input("How many coins do you think there are? "))
+                    else:
+                        print("got to else")
+                        guessing = False
+                        break
         players[j].makeAGuess(guess)
 
     for j in range(0, len(players)):
-        print("Player " + players[j].name + " guessed " + players[j].guess)
+        print("Player " + players[j].name + " guessed " + str(players[j].guess))
         total = 0
         for k in range(0, len(players)):
             total += int(players[k].coins)
         if int(players[j].guess) == total:
-            print(players[j].name + " wins with " + str(players[j].guess) + "!")
+            print(players[j].name + " wins!")
             players[j].addToScore()
         else:
-            print(players[j].name + " guessed incorrectly with " + str(players[j].guess))
+            print(players[j].name + " guessed incorrectly!")
 
 for i in range(0, len(players)):
     print("The score of " + str(players[i].name) + " was " + str(players[i].score))
