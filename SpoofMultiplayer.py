a4757a7ef7658c30db23b1f62a8725548e1478a7
chrishowes55 <chrishowes36@gmail.com>
2017-11-20 15:50:30 +0000

    Ready to take home

--- SpoofMultiplayer.py
+++ SpoofMultiplayer.py
@@ -5,13 +5,13 @@ class Player:
         self.name = name
         self.score = 0
 
-    def addToScore():
+    def addToScore(self):
         score += 1
 
-    def makeAGuess(guess):
+    def makeAGuess(self, guess):
         self.guess = guess
 
-    def chooseCoins(coins):
+    def chooseCoins(self, coins):
         self.coins = coins
 
 numberOfPlayers = int(input("How many players do you want?"))
@@ -26,3 +26,23 @@ rounds = int(input("How many rounds would you like to play? "))
 for i in range(0, rounds):
     for j in range(0, len(players)):
         print("Hello, " + players[j].name)
+        coins = input("How many coins do you have?")
+        players[j].chooseCoins(coins)
+        
+        guess = input("How many coins do you think there are?")
+        players[j].makeAGuess(guess)
+
+    for j in range(0, len(players)):
+        print("Player " + players[j].name + " guessed " + players[j].guess)
+        total = 0
+        for k in range(0, len(players)):
+            total += int(players[k].coins)
+        if players[j].guess == total:
+            print(players[j].name + " wins!")
+            players[j].addToScore()
+        else:
+            print(players[j].name + " guessed incorrectly")
+
+for i in range(0, len(players)):
+    print("The score of " + players[j].name + " was " + players[j].score)
+        
