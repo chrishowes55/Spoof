5a4f9e15e112c9fd9d5e589e6510fcb23e0f01e3
chrishowes55 <chrishowes36@gmail.com>
2017-11-20 15:34:26 +0000

    Gone Object-Oriented

--- SpoofMultiplayer.py
+++ SpoofMultiplayer.py
@@ -1,6 +1,28 @@
 import random
 
-numberOfPlayers = input("How many players do you want?")
-playerGuesses = []
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+    def addToScore():
+        score += 1
+
+    def makeAGuess(guess):
+        self.guess = guess
+
+    def chooseCoins(coins):
+        self.coins = coins
+
+numberOfPlayers = int(input("How many players do you want?"))
+players = []
 for i in range(0, numberOfPlayers):
-    playerGuesses.append(0)
+    name = input("Hello, Player! What should I call you?")
+    players.append(Player(name))
+    
+
+rounds = int(input("How many rounds would you like to play? "))
+
+for i in range(0, rounds):
+    for j in range(0, len(players)):
+        print("Hello, " + players[j].name)
